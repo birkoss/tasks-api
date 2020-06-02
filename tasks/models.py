37cb50088caa0cf0ee5b2e40bc866b4340dd15b6
Mathieu Robichaud <mathieu@standish.ca>
2020-06-02 14:47:01 -0400

    Basic models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+from users.models import Group, User
+
+
+class Task(TimeStampedModel, models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default='')
+    description = models.TextField(max_length=200, default='')
+
+    #frequency = 
+
+    reward = models.IntegerField(default=0)
+
+
+class TaskUser(TimeStampedModel, models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    date_validated = models.DateTimeField(null=True)
