72a1aa4b8ca7a1b8077f3a6c7f28aea432c60f57
Mathieu Robichaud <mathieu@standish.ca>
2020-06-07 15:05:17 -0400

    Add Task

--- tasks/api/views.py
+++ tasks/api/views.py
@@ -31,3 +31,25 @@ class tasksList(APIView):
         return Response({
             'tasks': serializer.data,
         }, status=status.HTTP_200_OK)
+
+    def post(self, request, group_pk, format=None):
+        group = Group.objects.filter(
+            id=group_pk, groupuser__user=request.user).first()
+
+        if group is None:
+            return Response({
+                'message': "Not found",
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(group=group, user=request.user)
+
+            return Response({
+                'task': serializer.data,
+                'status': status.HTTP_200_OK
+            })
+        else:
+            return Response({
+                'message': serializer.errors,
+            }, status=status.HTTP_404_NOT_FOUND)
