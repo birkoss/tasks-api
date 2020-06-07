2c4f077eb63dced6c75157026bfc6757764f5974
Mathieu Robichaud <mathieu@standish.ca>
2020-06-07 08:25:48 -0400

    Basic Task view

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 from users.api.urls import urlpatterns as users_urlpatterns
+from tasks.api.urls import urlpatterns as tasks_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + users_urlpatterns
+] + users_urlpatterns + tasks_urlpatterns
---
+++ tasks/api/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import login, authenticate
+
+from rest_framework import status, authentication, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..models import Task
+from users.models import Group, GroupUser
+
+from .serializers import TaskSerializer
+
+
+class tasksList(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, group_pk, format=None):
+        group = Group.objects.filter(
+            id=group_pk, groupuser__user=request.user).first()
+
+        if group is None:
+            return Response({
+                'message': "Not found",
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        tasks = Task.objects.filter(group=group)
+
+        serializer = TaskSerializer(tasks, many=True)
+
+        return Response({
+            'tasks': serializer.data,
+        }, status=status.HTTP_200_OK)
